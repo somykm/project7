7ad55a44329fd5e1b25def37d8c61a367d403f7d
https://github.com/somykm <sk43@hood.edu>
2024-11-18 00:17:47 -0500

    instal nodemon/dotenv/ express in backend

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Successfully connected to mongoDB Atlas');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB atlas!');
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
+
+
+
